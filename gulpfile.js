9c6391ff9d7dd1a73cc8f753025ec608602a612b
Diomedes <diomedes.lajara@gmail.com>
2018-01-08 03:35:11 -0500

    completed beginning section

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,44 @@
-var gulp = require('gulp');
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
 
-gulp.task('default', function() {
-  console.log('jelllowww from gulp cli');
-});
+// MOVED ALL TASK TO SEPARATE FILES
+// var gulp = require('gulp');
+// watch  = require('gulp-watch'),
+// browserSync = require('browser-sync').create();
+// postcss = require('gulp-postcss'),
+// autoprefixer = require('autoprefixer'),
+// cssvars = require('postcss-simple-vars'),
+// nested = require('postcss-nested'),
+// cssImport = require('postcss-import'),
 
-gulp.task('html', function() {
-  console.log("something else from gulp");
-}); 
+// CSS PIPE TASK moved to gulp/styles.js
+// gulp.task('styles', function() {
+//   return gulp.src('app/assets/styles/styles.css')
+//              .pipe(postcss([cssvars, cssImport, nested, autoprefixer]))
+//              .pipe(gulp.dest('app/temp/styles'));
+// }); 
 
-gulp.task('watch', function() {
-  watch('../app/index.html', function() {
-    gulp.start('html');
-  })
-  
-});
+// WATCH TASK moved to gulp/watch.js
+// gulp.task('watch', function() {
+
+//   browserSync.init({
+//     notify: false,
+//     server: {
+//       baseDir: 'app'
+//     }
+//   });
+
+//   watch('app/index.html', function() {
+//     browserSync.reload();
+//   });
+
+//   watch('app/assets/styles/**/*.css', function() {
+//     gulp.start('cssInject');
+//   });
+// });
+
+// CSS INJECT TASK
+// gulp.task('cssInject', ['styles'], function() {
+//   return gulp.src('app/temp/styles/styles.css')
+//       .pipe(browserSync.stream());
+// });
